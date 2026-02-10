453c256baf47a5efbe0f9bc9f807c27e9f6ae1a4
Trevor O'Dwyer <trevorodwyer619@hotmail.com>
2026-02-10 08:06:12 +0000

    Add style for desktop navigation

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -4,3 +4,14 @@ AOS.init({
   duration: 1500,
   once: false,
 });
+
+// Change navbar background on scroll
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+
+  if (window.scrollY > 0) {
+    navbar.classList.add("navbar-scroll");
+  } else {
+    navbar.classList.remove("navbar-scroll");
+  }
+});
