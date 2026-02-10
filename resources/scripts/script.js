7d31b3b4fae8b2a74454c3af18d8fd31483dd49a
Trevor O'Dwyer <trevorodwyer619@hotmail.com>
2026-02-10 08:28:57 +0000

    Add mobile nav toggle

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -5,6 +5,15 @@ AOS.init({
   once: false,
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.querySelector(".navbar .mobile-menu-toggle");
+  const mobileMenu = document.querySelector(".navbar .mobile-menu-items");
+
+  toggleButton.addEventListener("click", () => {
+    mobileMenu.classList.toggle("active");
+  });
+});
+
 // Change navbar background on scroll
 window.addEventListener("scroll", function () {
   const navbar = document.querySelector(".navbar");
